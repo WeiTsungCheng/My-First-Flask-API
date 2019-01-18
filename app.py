152c90e1701f24ffb91d5f25af890532aa5f0cbe
it-william <william.cheng@kkday.com>
2019-01-18 18:07:20 +0800

    t11

--- app.py
+++ app.py
@@ -6,14 +6,13 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
-
+print('0003')
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'jose'
 api = Api(app)
-
 
 # @app.route('/', methods=['GET', 'POST'])
 # def create():
@@ -30,5 +29,6 @@ api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
     from db import db
+    print('0004')
     db.init_app(app)
     app.run(port=5000, debug=True)
--- run.py
+++ run.py
@@ -2,7 +2,8 @@ from app import app
 from db import db
 
 db.init_app(app)
-
+print('0002')
 @app.before_first_request
 def create_tables():
+    print('0001')
     db.create_all()
