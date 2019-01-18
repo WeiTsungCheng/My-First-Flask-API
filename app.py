556175ffe43bb65a415e71616ee208b5c20a40ae
it-william <william.cheng@kkday.com>
2019-01-18 19:29:19 +0800

    Added PostgreSQL enviroment variable and psycopg2

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+# 因為 Heroku 存取 url 需要使用環境變數，所以import os
+import os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -8,7 +11,13 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 print('0003')
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+
+# 把原本的SQLite換成 PostgresSQL， 因為Heroku上，每隔一段時間它會結束應用，所以建立的資料都不會留下
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+# 這段將會去問運作系統 有沒有DATABASE_URL 這個參數，如果我們正在運行Heroku，將會為我們連結上postgre app
+# 為了讓在本地端開發的時候 還是可以使用sqlite ，所以把sqlite 的參數放在第二個，如果第一個讀不到才會找第二個）
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'jose'
--- run.py
+++ run.py
@@ -2,8 +2,8 @@ from app import app
 from db import db
 
 db.init_app(app)
-print('0002')
+print('0001')
 @app.before_first_request
 def create_tables():
-    print('0001')
+    print('0002')
     db.create_all()
